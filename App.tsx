bb1d8f737a44498bbb6cc0b18e21d065fe586c59
admin <admin@MacBook-Pro-Petr-2.local>
2022-02-04 05:10:16 +0300

    init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, Button, Image, StyleSheet, Text, View} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import {useState} from "react";
+
 
 export default function App() {
+  const [pickerStateResult, setPickerStateResult] = useState(undefined);
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      Alert.alert("Сейчас вы не можете загрузить фотографии", "Перейдите в настройки устройства и разрешите приложению доступ к изображениям");
+      return;
+    }
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+    console.log('heregoesresult', pickerResult);
+    setPickerStateResult(pickerResult);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title={'Change photo'} onPress={openImagePickerAsync}/>
+      {pickerStateResult && <Image width={100} height={100} source={{uri: pickerStateResult.uri, width: 100, height: 100}} />}
     </View>
   );
 }
